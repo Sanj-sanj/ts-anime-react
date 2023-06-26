7fb489f5caf1815c0aa50ef1cacbaa8e828cebb2
Sanj-sanj <sanjeetu3@gmail.com>
2023-06-26 02:47:50 -0700

    Reoworked the data struct of the calendar cards, now will group by day and each day will group by air date of next episode with an array of shows inside the group key using dayjs format of 'ddd MM DD'

--- src/components/pages/Calendar/container/CalendarContainer.tsx
+++ src/components/pages/Calendar/container/CalendarContainer.tsx
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import UserListPreferences from "../../UserList/preferenceBar/UserListPreference";
 import { useStateContext } from "../../../../utilities/Context/AppContext";
 import { MainCard } from "../../../../interfaces/apiResponseTypes";
+import { sortAndFilterCardsForView } from "../../../../utilities/Cards/CardContainerUtils";
 
 const CalendarContainer = () => {
   // make a function to generate a array of 7 calendar dates for timetable view:
@@ -15,71 +16,103 @@ const CalendarContainer = () => {
   } = useStateContext();
 
   const ongoingToGroupedByDay = () => {
-    let temp: MainCard[];
-    if (showOngoing) {
-      temp = cards.ONGOING[format].concat(
-        cards[season]?.[seasonYear]?.[format]
-      );
-    } else {
-      temp = cards[season]?.[seasonYear]?.[format];
-    }
-    return temp.reduce(
+    const sortedByCountdown = sortAndFilterCardsForView(
+      "Countdown",
+      200,
+      { cards },
+      { season, format, seasonYear },
+      showOngoing
+    );
+    return sortedByCountdown.reduce(
       (acc, card) => {
-        const hasDate =
+        const cardDate =
           card.nextAiringEpisode?.airingAt &&
           dayjs(card.nextAiringEpisode?.airingAt * 1000);
-        if (hasDate)
-          return { ...acc, [hasDate.day()]: [...acc[hasDate.day()], card] };
+
+        if (cardDate) {
+          acc[cardDate.day()] = {
+            entries: {
+              ...acc[cardDate.day()].entries,
+              [cardDate.format("ddd MMM DD")]: {
+                ...acc[cardDate.day()].entries[cardDate.format("ddd MMM DD")],
+                // date: cardDate,
+                shows: [
+                  ...(acc[cardDate.day()].entries?.[
+                    cardDate.format("ddd MMM DD")
+                  ]?.shows || []),
+                  card,
+                ],
+              },
+            },
+            // shows: [...acc[cardDate.day()].shows, card],
+            day: cardDate.get("day"),
+          };
+        }
         return acc;
       },
-      { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [] } as {
-        [n in number]: MainCard[];
-      }
+
+      [
+        { entries: {}, day: 0 },
+        { entries: {}, day: 1 },
+        { entries: {}, day: 2 },
+        { entries: {}, day: 3 },
+        { entries: {}, day: 4 },
+        { entries: {}, day: 5 },
+        { entries: {}, day: 6 },
+      ] as {
+        entries: {
+          [day in string]: { shows: MainCard[] };
+        };
+        day: number;
+      }[]
     );
   };
 
-  function genCalendarSlots() {
-    const showsGroupedByDay = ongoingToGroupedByDay();
-    return new Array(7).fill({}).map((_, i) => {
-      const date = dayjs().set("date", dayjs().get("date") + i);
+  const slotFramework = ongoingToGroupedByDay();
 
-      return {
-        day: date.get("day"),
-        shows: showsGroupedByDay[date.get("day")],
-        date: date,
-      };
-    });
-  }
-  const slotFramework = genCalendarSlots();
   const calendarByTimeline = (slots: typeof slotFramework) => {
-    console.log("i called");
     return (
       <div className="w-full bg-slate-800 flex justify-between">
-        {slots.map(({ day, shows, date }) => {
+        {slots.map(({ day, entries }) => {
           return (
             <div
               key={day}
               className="text-center w-full border-x border-t border-slate-500"
             >
-              <h2 className="text-3xl text-blue-400 ">
-                {date.format("ddd MMM MM")}
-              </h2>
-              {shows.map(({ title, id, coverImage }) => (
-                <div key={id} className="bg-slate-300 w-full">
-                  <div></div>
-                  <div className="w-full flex">
-                    <div className="w-2 bg-gray-400 flex items-center"></div>
-                    <div className="w-full flex">
-                      <img
-                        src={coverImage.medium || ""}
-                        alt=""
-                        className="w-12 h-[72px]"
-                      />
-                      <h3>{title[titlesLang] || title.romaji}</h3>
-                    </div>
-                  </div>
-                </div>
-              ))}
+              {Object.entries(entries).map(([str, { shows }]) => {
+                return (
+                  <>
+                    <h2
+                      key={str}
+                      className={`text-3xl text-blue-400 ${
+                        dayjs().format("ddd MMM DD") === str
+                          ? "bg-teal-600 text-pink-50"
+                          : ""
+                      }`}
+                    >
+                      {str}
+                    </h2>
+
+                    {shows.map(({ title, id, coverImage }) => (
+                      <div key={id} className="bg-slate-300 w-full">
+                        <div className="w-full flex">
+                          <div className="w-2 bg-gray-400 flex items-center"></div>
+                          <div className="w-full flex">
+                            <img
+                              src={coverImage.medium || ""}
+                              alt=""
+                              className="w-12 h-[72px]"
+                            />
+                            <h3 className="text-center w-full text-ellipsis line-clamp-3 overflow-hidden">
+                              {title[titlesLang] || title.romaji}
+                            </h3>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                );
+              })}
             </div>
           );
         })}
