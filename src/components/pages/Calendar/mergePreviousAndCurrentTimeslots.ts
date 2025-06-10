ed069a2c8c3c891ee5276741dcc9a722709bcbb9
Sanjeet Sidhu <Sanjeetu3@gmail.com>
2025-06-10 14:39:51 -0700

    edited function to add todays unaired shows into the todays previously aired shows. need a lot of optional chaining otherwise we break things in movies or ovas if they dont exist that week. (unlikely in tv because something is always airing, but it covers that too)

--- src/components/pages/Calendar/mergePreviousAndCurrentTimeslots.ts
+++ src/components/pages/Calendar/mergePreviousAndCurrentTimeslots.ts
@@ -4,9 +4,14 @@ export default function mergePreviousAndCurrentTimeslots(
     previouslyAired: CalendarTimeSlots,
     currentAiring: CalendarTimeSlots
 ) {
-    return currentAiring.forEach((timeSlot, i) => {
+    currentAiring.forEach((timeSlot, i) => {
+        const date = timeSlot?.entries?.[0]?.date;
+        if(previouslyAired[i]?.entries?.[0]?.date === date) {
+            timeSlot.entries?.[0]?.shows?.unshift(...previouslyAired?.[i]?.entries?.[0]?.shows || [])
+            return;
+        }
         if(previouslyAired[i].entries[0]) {
             timeSlot.entries.unshift(previouslyAired[i].entries[0])
         }
-    })
+    }) 
 }
